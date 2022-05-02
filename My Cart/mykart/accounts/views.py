ec1914849540a16ba374f5da59a63e4f32a23993
Shaun47 <hossain.shaun27@gmail.com>
2022-05-02 20:54:15 +0600

    done till place order

--- My Cart/mykart/accounts/views.py
+++ My Cart/mykart/accounts/views.py
@@ -18,7 +18,7 @@ from django.utils.encoding import force_bytes,force_text
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.core.mail import EmailMessage
 from carts.views import cart_ID
-from carts.models import Cart, CartItem
+from carts.models import Cart, CartItem 
 
 
 
